939ac34c606c196b1bf0569fa03c9d9ceba06a72
vansh031 <125873151+vansh031@users.noreply.github.com>
2023-11-19 21:48:32 -0800

    Add files via upload

---
+++ EXP.1(b).py
@@ -0,0 +1,24 @@
+'''Demonstrate the use of following built-in-functions in python:
+abs(), len(), min(), round(), isalnum(), type()'''
+
+# The abs() function returns the absolute value of a number.
+print(abs(-10))
+
+#the len() function returns the length of a string or a sequence.
+print(len("Welcome to MIET!"))
+
+#The min() function returns the minimum value of a function.
+print(min([10,20,30]))
+
+#The round() function rounds a number to a specified number of decimal places.
+print(round(3.15422,2))
+
+#The isanum() returns True if a string contains only alphanumeric characters, False otherwise.
+print("CSE, 3rd sem ".isalnum())
+print("123abc".isalnum())
+
+#The type() function returns the type of an object.
+print(type(10))
+print(type("AI and, ML!"))
+
+
