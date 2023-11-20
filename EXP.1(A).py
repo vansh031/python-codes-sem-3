2ab09ddc958a9afdde49ae570fe43f9baf3517be
vansh031 <125873151+vansh031@users.noreply.github.com>
2023-11-19 21:37:20 -0800

    Add files via upload

---
+++ EXP.1(A).py
@@ -0,0 +1,38 @@
+
+# DataType Output: str
+x = "Welcome to CSE Department"
+print("DataType Output:", type(x))
+
+# DataType Output: int
+x = 50
+print("DataType Output:", type(x))
+
+# DataType Output: float
+x = 60.5
+print("DataType Output:", type(x))
+
+# DataType Output: complex
+x = 3j
+print("DataType Output:", type(x))
+
+# DataType Output: list
+x = ["Python", "Programming", "Language"]
+print("DataType Output:", type(x))
+
+# DataType Output: tuple
+x = ("MIET", "CSE", "Students")
+print("DataType Output:", type(x))
+
+# DataType Output: range
+x = range(10)
+print("DataType Output:", type(x))
+
+# DataType Output: dict
+x = {"name": "Kartik", "age": 21}
+print("DataType Output:", type(x))
+
+# DataType Output: set
+x = {"Data", "Types", "Python"}
+print("DataType Output:", type(x))
+
+
